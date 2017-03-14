3f31ae6fed8a853c505009a849a99030b747aba6
Elizabeth Davis <elizabethsdavis95@gmail.com>
2017-03-14 01:35:30 -0700

    configure angular app

---
+++ mainController.js
@@ -0,0 +1,10 @@
+'use strict';
+
+var unshackledApp = angular.module('unshackledApp', ['ngMaterial']);
+
+unshackledApp.controller('MainController', ['$scope', '$rootScope',
+  function ($scope, $rootScope) {
+
+    
+}]);
+
