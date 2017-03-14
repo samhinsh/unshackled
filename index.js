8189e887a55d7b8a63d0bae113d9be75f7c21edf
Samuel Hinshelwood Jr <samhinsh@gmail.com>
2017-03-13 17:13:42 -0700

    Initialized server

---
+++ index.js
@@ -0,0 +1,22 @@
+/* Web App for Unshackled app */
+
+
+/// MARK: - Server Initialization
+var express = require('express');
+var app 	= express();
+var port 	= 3000;
+
+app.use(express.static(__dirname));
+
+
+var server = app.listen(port, function() {
+	console.log(`Listening on ${port}`);
+});
+
+
+process.on('SIGTERM', function() {
+	console.log("Shutting server down.");
+	server.close(function() {
+		process.exit(0);
+	});
+});
