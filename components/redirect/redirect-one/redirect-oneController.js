ca70e4abeb9741c4a29c368f2b2caa0c016af60c
Elizabeth Davis <elizabethsdavis95@gmail.com>
2017-03-14 03:11:51 -0700

    redirect flow popup one

--- components/redirect/redirect-one/redirect-oneController.js
+++ components/redirect/redirect-one/redirect-oneController.js
@@ -1,9 +1,17 @@
 'use strict';
 
-unshackledApp.controller('RedirectOneController', ['$scope', '$rootScope',
-  function ($scope, $rootScope) {
+unshackledApp.controller('RedirectOneController', ['$scope', '$rootScope', '$timeout', '$mdBottomSheet', '$mdToast',
+  function ($scope, $rootScope, $timeout, $mdBottomSheet, $mdToast) {
 
-
+  	$scope.showListBottomSheet = function() {
+    	$scope.alert = '';
+    	$mdBottomSheet.show({
+	      templateUrl: 'components/redirect/redirect-one-popup/redirect-one-popupTemplate.html',
+	      controller: 'RedirectOnePopupController'
+    	}).then(function(clickedItem) {
+      		$scope.alert = clickedItem + ' clicked!';
+    	});
+  	}();
 
 
 
--- mainController.js
+++ mainController.js
@@ -1,6 +1,18 @@
 'use strict';
 
-var unshackledApp = angular.module('unshackledApp', ['ngMaterial']);
+var unshackledApp = angular.module('unshackledApp', ['ngMaterial', 'ngRoute', 'ngMessages']);
+
+unshackledApp.config(['$routeProvider',
+    function ($routeProvider) {
+        $routeProvider.
+        when('/redirect', {
+            templateUrl: 'components/redirect/redirect-one/redirect-oneTemplate.html',
+            controller: 'RedirectOneController'
+        }).
+        otherwise({
+            redirectTo: '/redirect'
+        });
+    }]);
 
 unshackledApp.controller('MainController', ['$scope', '$rootScope',
   function ($scope, $rootScope) {
