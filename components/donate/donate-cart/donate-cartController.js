56d36975c4edc3b569e8babcf7a33ca003020a6a
Elizabeth Davis <elizabethsdavis95@gmail.com>
2017-03-17 04:56:12 -0700

    add things

--- components/donate/donate-cart/donate-cartController.js
+++ components/donate/donate-cart/donate-cartController.js
@@ -3,27 +3,12 @@
 unshackledApp.controller('DonateCartController', ['$scope', '$rootScope', '$window', '$location', '$timeout', '$mdToast',
 	function ($scope, $rootScope, $window, $location, $timeout, $mdToast) {
 
-		$scope.showWincoIsGoodToast = function() {
-
-			var x = document.getElementById("winco-is-good-toast")
-
-			// Add the "show" class to DIV
-    		x.className = "show";
-
-    		// After 3 seconds, remove the show class from DIV
-    		setTimeout(function(){ 
-    			x.className = x.className.replace("show", ""); 
-    			// $scope.showDonatePleaseToast();
-
-    		}, 3000);
-
-		}();
-
-		$scope.buttonClick = function() {
+		$scope.donate = function() {
 			console.log('yes, still registers');
+			$scope.showTellYourFriendsToast();
 			var win = $window.open('https://www.paypal.com/cgi-bin/webscr?cmd=_s-xclick&hosted_button_id=3A9URHKC8XYWS', '_blank');
   			win.focus();
-  			$scope.showTellYourFriendsToast();
+  			
 		}
 
 		$scope.showDonatePleaseToast = function() {
